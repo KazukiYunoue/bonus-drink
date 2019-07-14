9279a3fe2c5b9654efed3a52c21c615b2031a34b
KazukiYunoue <you.know.ue1480@gmail.com>
2019-07-14 17:32:37 +0900

    First update

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,21 @@
 class BonusDrink
   def self.total_count_for(amount)
-    0
+    raise ArgumentError.new("Negative amount size") unless amount >= 0
+
+    total_earned_bottles = empty_bottles = amount
+    while get_bonus_by?(empty_bottles) do
+      total_earned_bottles += get_bottles_and_remaining_empty_bottles_by(empty_bottles)[0]
+      empty_bottles = get_bottles_and_remaining_empty_bottles_by(empty_bottles).sum
+    end
+    total_earned_bottles
+  end
+
+  def self.get_bottles_and_remaining_empty_bottles_by(empty_bottles)
+    empty_bottles.divmod(3)
+  end
+
+  def self.get_bonus_by?(empty_bottles)
+    get_bottles_and_remaining_empty_bottles_by(empty_bottles)[0] > 0 ?
+      true : false
   end
 end
--- spec/bonus_drink_spec.rb
+++ spec/bonus_drink_spec.rb
@@ -1,5 +1,31 @@
 require File.expand_path(File.dirname(__FILE__) + '/../bonus_drink')
 
 describe BonusDrink do
-  specify { expect(BonusDrink.total_count_for(0)).to eq 0 }
+  describe "#total_count_for" do
+    let(:total_count_for) { BonusDrink.total_count_for(number) }
+    context "when argument is 0" do
+      let(:number) { 0 }
+      it { expect(total_count_for).to eq 0 }
+    end
+    context "when argument is 1" do
+      let(:number) { 1 }
+      it { expect(total_count_for).to eq 1 }
+    end
+    context "when argument is 3" do
+      let(:number) { 3 }
+      it { expect(total_count_for).to eq 4 }
+    end
+    context "when argument is 11" do
+      let(:number) { 11 }
+      it { expect(total_count_for).to eq 16 }
+    end
+    context "when argument is 100" do
+      let(:number) { 100 }
+      it { expect(total_count_for).to eq 149 }
+    end
+    context "when argument is -1" do
+      let(:number) { -1 }
+      it { expect { total_count_for }.to raise_error(ArgumentError) }
+    end
+  end
 end
